a18d4a16135e3b593628ff028ebe912a98cf563b
Weliton Soares Pineuril <welitonsp@gmail.com>
2026-03-17 14:26:05 -0300

    fix: removendo testes obsoletos do Amadeus

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-# Ajusta o sys.path para incluir a raiz do repositório nos imports dos testes
-import os
-import sys
-
-repo_root = os.path.dirname(os.path.dirname(__file__))
-if repo_root not in sys.path:
-    sys.path.insert(0, repo_root)
